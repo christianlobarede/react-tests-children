a1fb14d605787c46ceb16c358793a620422dcbdd
Christian Lobarede <ch.lobarede@gmail.com>
2021-07-15 00:58:31 -0400

    Children Components props

---
+++ src/components/Subcontents.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Subcontents extends Component {
+  render() {
+    return <div className="subcontents"></div>;
+  }
+}
+
+export default Subcontents;
